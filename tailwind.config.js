de1f79bc8b374267b3ca286e3e8aae18f4153179
AnnaErs <annaershova02@gamil.com>
2023-05-05 17:38:18 +0500

    feat: BIKDARTEM-116: [FRONTEND] Добавить стили в UI kit (размер шрифтов и цвета)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,89 @@
 export default {
   content: ["**/*.{ts,tsx,html}"],
   theme: {
-    extend: {},
+    fontSize: {
+      'header': [
+        '36px',
+        {
+          lineHeight: '54px',
+          fontWeight: '700'
+        }
+      ],
+      'subtitle': [
+        '24px',
+        {
+          lineHeight: '36px',
+          fontWeight: '700'
+        }
+      ],
+      'paragraph': [
+        '16px',
+        {
+          lineHeight: '24px',
+          fontWeight: '400'
+        }
+      ],
+      'paragraph2': [
+        '20px',
+        {
+          lineHeight: '26px',
+          fontWeight: '400'
+        }
+      ],
+      'small': [
+        '12px',
+        {
+          lineHeight: '16px',
+          fontWeight: '400'
+        }
+      ],
+      'bold': [
+        '16px',
+        {
+          lineHeight: '24px',
+          fontWeight: '700'
+        }
+      ],
+      'bold2': [
+        '20px',
+        {
+          lineHeight: '26px',
+          fontWeight: '700'
+        }
+      ],
+      'hero': [
+        '48px',
+        {
+          lineHeight: '64px',
+          fontWeight: '700'
+        }
+      ],
+    },
+    extend: {
+      colors: {
+        // Common - цвета используются только для фона блоков или текстов
+        'common-black': '#000000',
+        'common-darkest-gray': '#131313',
+        'common-bg':'#222222',
+        'common-light-gray':'#C6C6C6',
+        'common-white':'#FFFFFF',
+        'common-gray':'#838383',
+        'common-dark-gray':'#424242',
+        // Primary - цвет обозначающий действие, если элемент является интерактивным, то нужно добавить обозначение данным цветом
+        'primary-light':'#68ADFD',
+        'primary-normal':'#3691FC',
+        'primary-dark':'#0476FB',
+        // Success - цвет обозначающий согласие с происходящим
+        'success-light':'#66FF9A',
+        'success-normal':'#35FF7A',
+        // Danger - цвет обозначающий о том что действие опасно и нужно отнестись с вниманием
+        'dander-light':'#EF1060',
+        'dander-normal':'#BF0D4D',
+        // Warning - цвет обозначает предупреждение что нужно ознакомиться с происходящим
+        'warning-light':'#F6E96F',
+        'warning-normal':'#F3E13F',
+      },
+    },
   },
   plugins: [],
 }
