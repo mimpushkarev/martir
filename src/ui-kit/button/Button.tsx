5044b8c87a80e15502614542a24e5fa2c68bd5c6
AnnaErs <annaershova02@gamil.com>
2023-05-08 18:33:00 +0500

    feat: BIKDARTEM-117: [FRONTEND] Добавить компонент кнопки

---
+++ src/ui-kit/button/Button.tsx
@@ -0,0 +1,31 @@
+import { memo } from "react";
+
+import { cn } from "@utils/cn";
+
+import { ButtonPropsType } from "./types";
+
+const Button = memo<ButtonPropsType>(({ children, type, state, ...props }) => {
+    return (
+        <button
+            {...props}
+            disabled={props.disabled || state === "disabled"}
+            className={cn(
+                "text-paragraph px-5 py-2 rounded ease-in-out duration-200 disabled:opacity-50 disabled:cursor-not-allowed",
+                {
+                    ["bg-primary-normal text-white hover:bg-primary-light"]:
+                        type === "primary",
+                    ["bg-success-normal text-black hover:bg-success-light"]:
+                        type === "success",
+                    ["bg-danger-normal text-white hover:bg-danger-light"]:
+                        type === "danger",
+                    ["bg-warning-normal text-black hover:bg-warning-light"]:
+                        type === "warning",
+                }
+            )}
+        >
+            {children}
+        </button>
+    );
+});
+
+export default Button;
