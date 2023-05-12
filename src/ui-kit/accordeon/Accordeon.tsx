8827a6db8a110fdd99efba97306b73cb3f2f2501
AnnaErs <annaershova02@gamil.com>
2023-05-12 17:01:01 +0500

    review fix

--- src/ui-kit/accordeon/Accordeon.tsx
+++ src/ui-kit/accordeon/Accordeon.tsx
@@ -2,13 +2,15 @@ import { memo } from "react";
 import { Disclosure } from "@headlessui/react";
 import { ChevronUpIcon } from "@heroicons/react/20/solid";
 
+import { AccordeonType } from "./types";
+
 const accordeonText = {
     accordionTitle: "Accordion",
     accordionSubTitle:
         "Accordion Subtitle Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel placerat neque, a posuere magna. Integer leo turpis, interdum cursus nibh sed, tincidunt fringilla dolor. Etiam fringilla viverra leo eu eleifend. Fusce porttitor risus vitae metus ultrices, et pulvinar nibh elementum. Nunc in ullamcorper leo. In eu orci elit. Donec accumsan est libero, bibendum fermentum libero consectetur sed. Duis leo nulla, volutpat at metus a, faucibus lobortis nisl. Quisque ipsum felis, tincidunt laoreet ultrices at, euismod a odio. Mauris massa eros, hendrerit eu imperdiet id, fermentum vitae orci.",
 };
 
-const Accordeon = () => {
+const Accordeon: AccordeonType = () => {
     return (
         <div className="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
             <Disclosure>
