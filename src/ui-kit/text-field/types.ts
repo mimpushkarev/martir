3f59d5b9996eb6c3e966ea12cb933c56e503e36a
AnnaErs <annaershova02@gamil.com>
2023-06-04 20:06:42 +0500

    feat: BIKDARTEM-119: [FRONTEND] Добавить компонент формы

---
+++ src/ui-kit/text-field/types.ts
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+type TextFieldPropsType = {
+    placeholder?: string;
+    value: string;
+    onChange: (value: string) => void;
+    disabled?:boolean;
+};
+
+export type TextFieldType = FC<TextFieldPropsType>;
