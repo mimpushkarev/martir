aa637a9b2e3efc0004daab4c8ce03d71f3b3100d
AnnaErs <annaershova02@gamil.com>
2023-08-21 18:16:34 +0500

    review fix

--- src/entities/price-tag/PriceTag.tsx
+++ src/entities/price-tag/PriceTag.tsx
@@ -1,8 +1,8 @@
 import {memo} from 'react';
 
 import {Button} from '_shared/button';
+import {Link} from '_shared/link';
 import {cn} from '_utils/cn';
-import {dividePrice} from '_utils/devide-price';
 
 import {PriceTagType} from './types';
 import img from './vector.svg';
@@ -16,15 +16,19 @@ const PriceTag = memo<PriceTagType>(function PriceTag({product, className}) {
     >
       <div className="mb-9 flex flex-col gap-2">
         <div className="text-paragraph-2">{product.name}</div>
-        <div className="text-subtitle">{dividePrice(product.price)} ₽</div>
+        <div className="text-subtitle">
+          {Intl.NumberFormat('rub', {minimumFractionDigits: 0, style: 'currency', currency: 'RUB'}).format(
+            Math.round(Number(product.price))
+          )}
+        </div>
       </div>
       <div className="flex flex-col gap-2">
-        <a href={product.size_guide_link} target="_blank" className="text-small text-common-light-gray underline">
+        <Link to={product.size_guide_link} target="_blank" className="text-common-light-gray underline">
           <div className="flex items-center gap-2">
             Руководство по выбору размера
             <img src={img} alt={product.size_guide_link} className=" -rotate-45" />
           </div>
-        </a>
+        </Link>
         <Button type="primary">Добавить в корзину</Button>
       </div>
     </div>
