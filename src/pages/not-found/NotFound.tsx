16b2a8a12ed730f132acb8ae984dfef5206c81de
AnnaErs <84527125+AnnaErs@users.noreply.github.com>
2023-10-10 13:56:31 +0500

    feat: BIKDARTEM-268: [FRONTEND] Сверстать страницу 404 (#50)
    Co-authored-by: AnnaErs <annaershova02@gamil.com>

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,9 +1,26 @@
-function NotFound() {
+import {memo} from 'react';
+
+import {Button} from '_shared/button';
+import {Container} from '_shared/container';
+import {Link} from '_shared/link';
+
+const NotFound = memo(function NotFound() {
   return (
-    <div>
-      <h1 className="text-lg">Page not found</h1>
-    </div>
+    <Container className="flex items-center justify-center ">
+      <div className="mt-[100px] flex w-1/3 flex-col items-center">
+        <h1 className="mb-10 text-page-title uppercase">Ой, товар не найден...</h1>
+        <p className="mb-6 text-paragraph-2">
+          Похоже, товар раскупили или у нас произошла ошибка... <br /> Но у нас много интересных товаров, возвращайтесь
+          в каталог и выберите подходящий.
+        </p>
+        <Link to={'/'}>
+          <Button type="primary">
+            <p className="px-9 text-paragraph">В каталог!</p>
+          </Button>
+        </Link>
+      </div>
+    </Container>
   );
-}
+});
 
 export default NotFound;
